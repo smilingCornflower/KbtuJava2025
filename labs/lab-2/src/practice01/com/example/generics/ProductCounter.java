3a65b9f0909f2caa5973f6e7771c76770bcda864
nauzybekturganbayev <smiling.cornflower@gmail.com>
2025-02-06 23:58:39 +0500

    lab 2 is ready

---
+++ labs/lab-2/src/practice01/com/example/generics/ProductCounter.java
@@ -0,0 +1,42 @@
+package practice01.com.example.generics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ProductCounter {
+    private Map<String, Long> productCountMap = new HashMap<>();
+    private Map<String, String> productNames = new TreeMap<>();
+
+    public static void main(String[] args) {
+        String[] parts = new String[]{"1S01", "1S01", "1S01", "1S01", "1S01", "1S02", "1S02", "1S02", "1H01", "1H01", "1S02", "1S01", "1S01", "1H01", "1H01", "1H01", "1S02", "1S02", "1M02", "1M02", "1M02"};
+
+        Map<String, String> productNames = new TreeMap<>();
+        productNames.put("Blue Polo Shirt", "1S01");
+        productNames.put("Black Polo Shirt", "1S02");
+        productNames.put("Red Ball Cap", "1H01");
+        productNames.put("Duke Mug   ", "1M02");
+
+        ProductCounter pc1 = new ProductCounter(productNames);
+        pc1.processList(parts);
+        pc1.printReport();
+    }
+
+    public ProductCounter(Map<String, String> productNames) {
+        this.productNames = productNames;
+    }
+
+    public void processList(String[] list) {
+        for (String itemNumber : list) {
+            productCountMap.put(itemNumber, productCountMap.getOrDefault(itemNumber, 0L) + 1);
+        }
+    }
+
+    public void printReport() {
+        System.out.println("=== Product Report ===");
+        for (String key : productNames.keySet()) {
+            System.out.print("Name: " + key);
+            System.out.println("\t\tCount: " + productCountMap.get(productNames.get(key)));
+        }
+    }
+}
---
+++ labs/lab-2/src/practice03/com/example/generics/InventoryCount.java
@@ -0,0 +1,10 @@
+package practice03.com.example.generics;
+
+public interface InventoryCount {
+
+    public long getCount();
+
+    public void addItems(long count);
+
+    public void removeItems(long count);
+}
